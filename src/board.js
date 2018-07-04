f7fca643bdc69d020b4aec24752e74df8538543f
Jay Shenk <jay.shenk@gmail.com>
2018-07-04 14:39:08 -0400

    add initial grid styles

--- src/board.js
+++ src/board.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import './board.css';
 
 class Board extends React.Component {
   render() {
     const cells = this.props.G.grid.map((row, i) => (
       row.map((cell, j) => {
         const id = 3 * i + j;
-        return <div key={id}>{this.props.G.grid[i][j]}</div>;
+        return (
+          <div className="cell" key={id}>
+            <div className="space">{this.props.G.grid[i][j]}</div>
+          </div>
+        );
       })
     ));
-    return cells;
+    return (
+      <div className="container">{cells}</div>
+    );
   }
 }
 
